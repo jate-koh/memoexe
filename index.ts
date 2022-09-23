2530a92a23783c60d8a47bcdf839442e4f0b6840
jate-koh <jate_ben@hotmail.com>
2022-09-24 00:13:06 +0700

    feat: add bot deployment

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-import DiscordJS from 'discord.js'
+import DiscordJS ,{ messageLink, GatewayIntentBits, Message } from 'discord.js'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const client =  new DiscordJS.Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
+    ]
+})
+
+client.on('ready', () => {
+    console.log('Bot Ready')
+})
+
+client.on('messageCreate', (message) => {
+    if(message.channel.id = '1022891851939852358') {
+        if(message.content == 'ping') {
+            message.reply ({
+                content: 'hello world'
+            })
+        }
+    }
+})
+
+client.login(process.env.TOKEN)
+
+
