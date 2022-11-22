60a30e73f2696272c27c9e4a86427fd43bf86251
jate-koh <jate_ben@hotmail.com>
2022-11-22 20:58:30 +0700

    [fix] console logger creating too much space

--- src/utils/ConsoleLogger.ts
+++ src/utils/ConsoleLogger.ts
@@ -12,66 +12,42 @@ export default class ConsoleLogger {
 
     public sendInformationLog(message: string, logger?: string): void {
         if (logger) {
-            console.log(`
-                ${logger} [${getCurrentFormatDate()}]: ${message}
-            `);
+            console.log(`${logger} [${getCurrentFormatDate()}]: ${message}`);
         } else if (this.logger) {
-            console.log(`
-                ${this.logger} [${getCurrentFormatDate()}]: ${message}
-            `);
+            console.log(`${this.logger} [${getCurrentFormatDate()}]: ${message}`);
         } else {
-            console.log(`
-                [${getCurrentFormatDate()}]: ${message}
-            `);
+            console.log(`[${getCurrentFormatDate()}]: ${message}`);
         }
     }
 
     public sendErrorLog(message: string, logger?: string): void {
         if (logger) {
-            console.log(`
-                ${logger} [${getCurrentFormatDate()}]: ${message}
-            `);
+            console.log(`${logger} [${getCurrentFormatDate()}]: ${message}`);
         } else if (this.logger) {
-            console.log(`
-                ${this.logger} [${getCurrentFormatDate()}]: ${message}
-            `);
+            console.log(`${this.logger} [${getCurrentFormatDate()}]: ${message}`);
         } else {
-            console.log(`
-                [${getCurrentFormatDate()}]: ${message}
-            `);
+            console.log(`[${getCurrentFormatDate()}]: ${message}`);
         }
     }
 
     public sendWarningLog(message: string, logger?: string): void {
         if (logger) {
-            console.log(`
-                ${logger} [${getCurrentFormatDate()}]: ${message}
-            `);
+            console.log(`${logger} [${getCurrentFormatDate()}]: ${message}`);
         } else if (this.logger) {
-            console.log(`
-                ${this.logger} [${getCurrentFormatDate()}]: ${message}
-            `);
+            console.log(`${this.logger} [${getCurrentFormatDate()}]: ${message}`);
         } else {
-            console.log(`
-                [${getCurrentFormatDate()}]: ${message}
-            `);
+            console.log(`[${getCurrentFormatDate()}]: ${message}`);
         }
     }
 
     public getError(message: string, logger?: string): Error {
         if (logger) {
-            return Error(`
-                ${logger} [${getCurrentFormatDate()}]: ${message}
-            `);
+            return Error(`${logger} [${getCurrentFormatDate()}]: ${message}`);
         }
         if (this.logger) {
-            return Error(`
-                ${this.logger} [${getCurrentFormatDate()}]: ${message}
-            `);
+            return Error(`${this.logger} [${getCurrentFormatDate()}]: ${message}`);
         }
-        return Error(`
-                [${getCurrentFormatDate()}]: ${message}
-            `);
+        return Error(`[${getCurrentFormatDate()}]: ${message}`);
     }
 
 }
