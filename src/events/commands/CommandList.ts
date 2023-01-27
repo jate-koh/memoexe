1cefedc6c5d75e63a8cdd252ea20e0befa4a9bb2
jate-koh <jate_ben@hotmail.com>
2023-01-27 15:41:10 +0700

    [feat] add test command for testing function

--- src/events/commands/CommandList.ts
+++ src/events/commands/CommandList.ts
@@ -3,11 +3,13 @@ import { SpecialCommand } from '@/events/commands/special/SpecialCommand';
 import Phasmo from '@/events/commands/generic/Phasmo';
 import Version from '@/events/commands/generic/Version';
 import Reload from '@/events/commands/special/Reload';
+import Test from '@/events/commands/generic/Test';
 
 const phasmo = new Phasmo();
 const version = new Version();
 const reload = new Reload();
+const test = new Test();
 
-export const commandList: GenericCommand[] = [phasmo, version];
+export const commandList: GenericCommand[] = [phasmo, version, test];
 
 export const specialCommandList: SpecialCommand[] = [reload];
---
+++ src/events/commands/generic/Test.ts
@@ -0,0 +1,45 @@
+import { CommandInteraction, EmbedBuilder } from 'discord.js';
+import {
+    SlashCommandBuilder,
+    SlashCommandSubcommandsOnlyBuilder,
+} from '@discordjs/builders';
+import packageJson from 'package.json';
+import { GenericCommand } from '@/events/commands/generic/GenericCommand';
+import ConsoleLogger from '@/utils/ConsoleLogger';
+import CommandOperator from '@/events/commands/CommandOperator';
+
+export default class Test extends GenericCommand {
+
+    private consoleLogger = new ConsoleLogger(this.constructor.name);
+
+    public data = new SlashCommandBuilder()
+        .setName('test')
+        .setDescription('Bot test command');
+
+    public run = async (interaction: CommandInteraction) => {
+        this.consoleLogger.sendInformationLog('Running Command...');
+
+        const { user } = interaction;
+
+        /* Insert test statement in this Region*/
+        const commandOps = new CommandOperator();
+        commandOps.readCommandsDir();
+        /* END TEST REGION */
+
+        const embedTestInfo = new EmbedBuilder();
+        embedTestInfo.setTitle('**Bot Test Command**');
+        embedTestInfo.setDescription(`Test: Done!`);
+        embedTestInfo.setAuthor({
+            name: user.username,
+            iconURL: user.displayAvatarURL(),
+        });
+        // await interaction.editReply({ embeds: [embedBotInfo] });
+
+        await interaction.reply({ embeds: [embedTestInfo] });
+    };
+
+    public getInstance(): object {
+        return this;
+    }
+
+}
