e5059376d13175d5009e9a4406c684d7ea5c0f3b
jate-koh <jate_ben@hotmail.com>
2023-01-31 23:57:47 +0700

    [refactor] change generic commands to normal type

--- src/events/commands/listing/Phasmo.ts
+++ src/events/commands/listing/Phasmo.ts
@@ -3,10 +3,10 @@ import {
     SlashCommandBuilder,
     SlashCommandSubcommandsOnlyBuilder,
 } from '@discordjs/builders';
-import { GenericCommand } from '@/events/commands/generic/GenericCommand';
 import ConsoleLogger from '@/utils/ConsoleLogger';
+import { Command } from '@/events/commands/Command';
 
-export default class Phasmo extends GenericCommand {
+export default class Phasmo extends Command {
 
     private consoleLogger = new ConsoleLogger(this.constructor.name);
 
--- src/events/commands/listing/Play.ts
+++ src/events/commands/listing/Play.ts
@@ -1,18 +1,34 @@
 import { CommandInteraction, EmbedBuilder } from 'discord.js';
 import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from '@discordjs/builders';
+import { } from '@discordjs/voice';
+
 import packageJson from 'package.json';
-import { GenericCommand } from '@/events/commands/generic/GenericCommand';
 import ConsoleLogger from '@/utils/ConsoleLogger';
 import CommandOperator from '@/events/commands/CommandOperator';
+import { Command } from '@/events/commands/Command';
 
-export default class Play extends GenericCommand {
+export default class Play extends Command {
 
     private consoleLogger = new ConsoleLogger(this.constructor.name);
 
     public data = new SlashCommandBuilder()
         .setName('play')
         .setDescription('Play a song')
-        .addStringOption((option) => option.setName('query').setDescription('Name or link of song to search'));
+        .addSubcommand((subcommand) => subcommand
+            .setName('song')
+            .setDescription('Loads a single song from url')
+            .addStringOption((option) => option
+                .setName('url').setDescription('Song\'s url').setRequired(true)))
+        .addSubcommand((subcommand) => subcommand
+            .setName('playlist')
+            .setDescription('Loads a playlist of songs from a url')
+            .addStringOption((option) => option
+                .setName('url').setDescription('Playlist\'s url').setRequired(true)))
+        .addSubcommand((subcommand) => subcommand
+            .setName('search')
+            .setDescription('Search for song based on provided keywords')
+            .addStringOption((option) => option
+                .setName('searchterms').setDescription('search\'s keywords').setRequired(true)));
 
     public run = async (interaction: CommandInteraction) => {
         this.consoleLogger.sendInformationLog('Running Command...');
@@ -26,8 +42,6 @@ export default class Play extends GenericCommand {
             name: user.username,
             iconURL: user.displayAvatarURL(),
         });
-        // await interaction.editReply({ embeds: [embedBotInfo] });
-
         await interaction.reply({ embeds: [embedMusicInfo] });
     };
 
