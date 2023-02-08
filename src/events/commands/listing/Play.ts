c45942184f39087c4f556770fc6c22c90e4ea833
jate-koh <jate_ben@hotmail.com>
2023-02-08 23:05:05 +0700

    [feat] add audio player utils

--- src/events/commands/listing/Play.ts
+++ src/events/commands/listing/Play.ts
@@ -1,11 +1,12 @@
 import { CommandInteraction, EmbedBuilder } from 'discord.js';
 import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from '@discordjs/builders';
-import { } from '@discordjs/voice';
+import { DiscordGatewayAdapterCreator, joinVoiceChannel } from '@discordjs/voice';
+import { Track } from 'discord-player';
 
-import packageJson from 'package.json';
+import AudioPlayer from '@/utils/AudioPlayer';
 import ConsoleLogger from '@/utils/ConsoleLogger';
-import CommandOperator from '@/events/commands/CommandOperator';
 import { Command } from '@/events/commands/Command';
+import AuthManager from '@/utils/AuthManager';
 
 export default class Play extends Command {
 
@@ -13,35 +14,63 @@ export default class Play extends Command {
 
     public data = new SlashCommandBuilder()
         .setName('play')
-        .setDescription('Play a song')
-        .addSubcommand((subcommand) => subcommand
-            .setName('song')
-            .setDescription('Loads a single song from url')
-            .addStringOption((option) => option
-                .setName('url').setDescription('Song\'s url').setRequired(true)))
-        .addSubcommand((subcommand) => subcommand
-            .setName('playlist')
-            .setDescription('Loads a playlist of songs from a url')
-            .addStringOption((option) => option
-                .setName('url').setDescription('Playlist\'s url').setRequired(true)))
-        .addSubcommand((subcommand) => subcommand
-            .setName('search')
-            .setDescription('Search for song based on provided keywords')
-            .addStringOption((option) => option
-                .setName('searchterms').setDescription('search\'s keywords').setRequired(true)));
+        .setDescription('Play a song');
+        // .addSubcommand((subcommand) => subcommand
+        //     .setName('song')
+        //     .setDescription('Loads a single song from url')
+        //     .addStringOption((option) => option
+        //         .setName('url').setDescription('Song\'s url').setRequired(true)));
+        // .addSubcommand((subcommand) => subcommand
+        //     .setName('playlist')
+        //     .setDescription('Loads a playlist of songs from a url')
+        //     .addStringOption((option) => option
+        //         .setName('url').setDescription('Playlist\'s url').setRequired(true)))
+        // .addSubcommand((subcommand) => subcommand
+        //     .setName('search')
+        //     .setDescription('Search for song based on provided keywords')
+        //     .addStringOption((option) => option
+        //         .setName('searchterms').setDescription('search\'s keywords').setRequired(true)));
 
-    public run = async (interaction: CommandInteraction) => {
+    public run = async (interaction: CommandInteraction, authProvider: AuthManager) => {
         this.consoleLogger.sendInformationLog('Running Command...');
 
         const { user } = interaction;
+        const client = authProvider.getBotClient();
+        const guild = client.guilds.cache.get(interaction.guildId);
+        const member = guild.members.cache.get(interaction.member.user.id);
+        const voiceChannel = member.voice.channel;
+
+        // const connection = joinVoiceChannel({
+        //     channelId: voiceChannel.id,
+        //     guildId: guild.id,
+        //     adapterCreator: guild.voiceAdapterCreator as DiscordGatewayAdapterCreator,
+        // });
+
+        let song: Track | undefined;
+        const audioPlayer = new AudioPlayer(authProvider);
+        audioPlayer.createQueue(guild.id, voiceChannel.id);
+        await audioPlayer.queryYouTubeMusic('creeper aw man', interaction.user.id)
+            .then((track) => {
+                song = track;
+            });
 
         const embedMusicInfo = new EmbedBuilder();
-        embedMusicInfo.setTitle('**Playing Music**');
-        embedMusicInfo.setDescription('null');
-        embedMusicInfo.setAuthor({
-            name: user.username,
-            iconURL: user.displayAvatarURL(),
-        });
+        if (song) {
+            embedMusicInfo.setTitle('**Playing Music**');
+            embedMusicInfo.setThumbnail(song.thumbnail);
+            embedMusicInfo.setDescription(`${song.title}`);
+            embedMusicInfo.setAuthor({
+                name: user.username,
+                iconURL: user.displayAvatarURL(),
+            });
+        } else {
+            embedMusicInfo.setTitle('**Song Not Found!**');
+            embedMusicInfo.setAuthor({
+                name: user.username,
+                iconURL: user.displayAvatarURL(),
+            });
+        }
+
         await interaction.reply({ embeds: [embedMusicInfo] });
     };
 
--- src/utils/AudioPlayer.ts
+++ src/utils/AudioPlayer.ts
@@ -1,5 +1,10 @@
-import { Player } from 'discord-player';
-import { Client } from 'discord.js';
+import {
+    Player, PlayerSearchResult, QueryType, Queue, Track,
+} from 'discord-player';
+import {
+    ChannelResolvable, Client, CommandInteraction,
+    GuildResolvable, UserResolvable,
+} from 'discord.js';
 
 import { MemoAuthManager as MainAuthManager } from '@/Memo';
 import ConsoleLogger from '@/utils/ConsoleLogger';
@@ -9,16 +14,56 @@ export default class AudioPlayer {
 
     private client: Client = MainAuthManager.getBotClient();
     private player: Player = undefined;
+    private queue: Queue = undefined;
     private consoleLogger = new ConsoleLogger(this.constructor.name);
+    private cacheResult: PlayerSearchResult = undefined;
 
     public constructor(authProvider?: AuthManager) {
         if (authProvider) this.client = authProvider.getBotClient();
+
+        // If not set, default to Main Auth Provider
         else this.client = MainAuthManager.getBotClient();
+
         this.init();
     }
 
-    public init() {
+    public init(): void {
         this.player = new Player(this.client);
+    }
+
+    public createQueue(guild: GuildResolvable, channel: ChannelResolvable) {
+        this.queue = this.player.createQueue(guild, {
+            metadata: {
+                // eslint-disable-next-line object-shorthand
+                channel: channel,
+            },
+        });
+    }
+
+    public async queryYouTubeMusic(url: string, user: UserResolvable): Promise<Track> {
+        let track: Track;
+        const result = await this.player.search(url, {
+            requestedBy: user,
+            searchEngine: QueryType.YOUTUBE_SEARCH,
+        }).then((response) => {
+            this.cacheResult = response;
+            track = this.getFirstResult();
+        });
+
+        return track;
+    }
+
+    public getFirstResult(): Track {
+        if (this.cacheResult) return this.cacheResult.tracks[0];
+        return undefined;
+    }
+
+    public playMusic(track: Track) {
+        this.queue.play(track);
+    }
+
+    public getResultCache(): PlayerSearchResult {
+        return this.cacheResult;
     }
 
 }
